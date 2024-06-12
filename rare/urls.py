8a4c0dd90d090384bfb7a41572b2d53937bf9dfe
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-06-12 11:22:04 -0500

    added comment views stuff, had to make a new branch to push

--- rare/urls.py
+++ rare/urls.py
@@ -25,12 +25,14 @@ from django.urls import path
 from rareapi.views.categories import CategoryView
 from rareapi.views.posts import PostView
 from rareapi.views.users import UserView
+from rareapi.views.comments import Comment
 from rareapi.views.auth import register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoryView, 'category')
 router.register(r'posts', PostView, 'post')
 router.register(r'users', UserView, 'user')
+router.register(r'comments', Comment, 'comment')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -2,4 +2,5 @@ from .categories import Category
 from .posts import Post
 from .reaction import Reaction
 from .users import User
+from .comments import Comment
 from .auth import register_user, check_user
